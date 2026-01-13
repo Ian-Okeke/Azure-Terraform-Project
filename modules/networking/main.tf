e2d2cab6d9b49f610470d7c57996281a4ebca04f
IAN <IAN@DESKTOP-5QKIJJU>
2026-01-13 23:01:01 +0000

    networking modules

---
+++ modules/networking/main.tf
@@ -0,0 +1,26 @@
+variable "vnet_name" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "address_space" { default = ["10.0.0.0/16"] }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.vnet_name}-subnet-1"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnet.id
+}
